456388111269ce6ad8a030b5c7a0a2dc0d5ac460
Stewart Wilson <stewart.wilson.c@gmail.com>
2018-08-01 14:36:28 -0700

    Flippers can move

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,29 +6,98 @@ public class InputController : MonoBehaviour {
 
     public GameObject leftFlipper;
     public GameObject rightFlipper;
-
+    public float flipDelay;
+    public float unflipDelay;
     public float flipSpeed;
     public float flipRotation;
+    public float flipDiff;
+
+
+    private bool leftFlipping;
+    private bool leftUnFlipping;
+    private bool rightFlipping;
+    private bool rightUnFlipping;
+    private float leftStartRotation;
+    private float rightStartRotation;
+    private float leftRotationTarget;
+    private float rightRotationTarget;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetButtonDown("Flip R"))
-        {
-            SwingFlipper(rightFlipper, -flipRotation);
-        }
-        if(Input.GetButtonDown("Flip L"))
-        {
-            SwingFlipper(leftFlipper, flipRotation);
-        }
-	}
+        leftFlipping = false;
+        rightFlipping = false;
 
-    private void SwingFlipper(GameObject _flipper, float _flipRotation)
+        leftStartRotation = leftFlipper.transform.rotation.eulerAngles.z;
+        rightStartRotation = rightFlipper.transform.rotation.eulerAngles.z;
+        leftRotationTarget = leftStartRotation;
+        rightRotationTarget = rightStartRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        _flipper.transform.Rotate(new Vector3(0, 0, _flipRotation), Space.Self);
+        if (!rightFlipping && Input.GetButtonDown("Flip R"))
+        {
+            StartCoroutine(FlipRight());
+        }
+        if (!leftFlipping && Input.GetButtonDown("Flip L"))
+        {
+            StartCoroutine(FlipLeft());
+        }
+
+        if(leftFlipping && Input.GetButtonUp("Flip L"))
+        {
+            StartCoroutine(UnFlipLeft());
+        }
+        if (leftFlipping && Input.GetButtonUp("Flip R"))
+        {
+            StartCoroutine(UnFlipRight());
+        }
+
+        if (rightFlipping)
+        {
+
+            rightFlipper.transform.rotation = Quaternion.RotateTowards(rightFlipper.transform.rotation, Quaternion.Euler(0, 0, rightRotationTarget), flipSpeed);
+            /*if (Mathf.Abs(rightRotationTarget - rightFlipper.transform.rotation.eulerAngles.z) % 360 < flipDiff)
+            {
+                rightRotationTarget = rightStartRotation;
+            }*/
+        }
+        if (leftFlipping)
+        {
+            leftFlipper.transform.rotation = Quaternion.RotateTowards(leftFlipper.transform.rotation, Quaternion.Euler(0, 0, leftRotationTarget), flipSpeed);
+            /*if (Mathf.Abs(leftRotationTarget - leftFlipper.transform.rotation.eulerAngles.z) % 360 < flipDiff)
+            {
+                leftRotationTarget = leftStartRotation;
+            }*/
+        }
+    }
+
+    IEnumerator FlipLeft()
+    {
+        leftFlipping = true;
+        leftRotationTarget = leftStartRotation + flipRotation;
+        yield return new WaitForSeconds(flipDelay);
+    }
+
+    IEnumerator UnFlipLeft()
+    {
+        leftRotationTarget = leftStartRotation;
+        yield return new WaitForSeconds(unflipDelay);
+        leftFlipping = false;
+    }
+
+    IEnumerator FlipRight()
+    {
+        rightFlipping = true;
+        rightRotationTarget = rightStartRotation - flipRotation;
+        yield return new WaitForSeconds(flipDelay);
+    }
+
+    IEnumerator UnFlipRight()
+    {
+        rightRotationTarget = rightStartRotation;
+        yield return new WaitForSeconds(unflipDelay);
+        rightFlipping = false;
     }
 }
